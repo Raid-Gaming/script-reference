9c8dfe6d218119b7f6780fcb97bc3f4f755dfda6
Jordy Rymenants <jordy.rymenants@outlook.com>
2020-12-20 16:43:04 +0100

    chore: set up PWA properties + icons

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,19 @@
 /* eslint-disable no-param-reassign */
 
+const appName = 'Scripting API - Raid Gaming';
+
 module.exports = {
   publicPath: '/',
   chainWebpack: (config) => {
     config.plugin('html').tap((args) => {
-      args[0].title = 'Scripting API - Raid Gaming';
+      args[0].title = appName;
       return args;
     });
+  },
+  pwa: {
+    name: appName,
+    themeColor: '#d4183b',
+    msTileColor: '#d4183b',
+    appleMobileWebAppCapable: 'yes',
   },
 };
